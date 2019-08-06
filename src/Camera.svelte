fb98db077f70489b5d431f9a1952c632b9a18b61
Makame Makame <makame-makame@outlook.com>
2019-08-06 18:26:01 +0200

    diplay camera stream on video element

--- src/Camera.svelte
+++ src/Camera.svelte
@@ -1,13 +1,20 @@
 <script>
 	async function startCamera(){
 		let stream = null
-		let constraints = {audio: false, video: true}
-		stream = await navigator.mediaDevices.getUserMedia(constraints)
+    let constraints = {audio: false, video: true}
+    try {
+      stream = await navigator.mediaDevices.getUserMedia(constraints)
+      const video = document.querySelector('video')
+      video.srcObject = stream
+    } catch (error) {
+      console.log('failed to connect with camera!', error)
+    }
+		
 	}
 </script>
 
 <style>
 
 </style>
-
+<video autoplay ></video>
 <button class="button" on:click="{startCamera}" >Verify</button>
